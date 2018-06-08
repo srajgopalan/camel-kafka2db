027b4ff17f0ed7954595d9d41ed53521d1155981
Shrikanth Rajgopalan <shrikanth.rajgopalan@gmail.com>
2018-06-08 15:28:08 +0800

    Added Simple Exception handling

--- src/main/java/com/srajgopalan/camel/route/Kafka2JdbcRoute.java
+++ src/main/java/com/srajgopalan/camel/route/Kafka2JdbcRoute.java
@@ -1,12 +1,18 @@
 package com.srajgopalan.camel.route;
 
+import com.srajgopalan.camel.exception.SimpleExceptionhandlerProcessor;
 import com.srajgopalan.camel.process.jdbc.SimpleJDBCInsertProcessor;
 import org.apache.camel.builder.RouteBuilder;
+import org.postgresql.util.PSQLException;
 
 public class Kafka2JdbcRoute extends RouteBuilder {
     public void configure() throws Exception {
 
-        from("kafka:test?brokers=localhost:9092&groupId=group1&consumersCount=1&autoOffsetReset=latest")
+        onException(PSQLException.class).handled(true).redeliveryDelay(2000).maximumRedeliveries(3)
+                .log("Exception Encountered while inserting messages to DB")
+                .process(new SimpleExceptionhandlerProcessor() );
+
+        from("kafka:my-topic?brokers=localhost:9092&groupId=group1&consumersCount=1&autoOffsetReset=latest")
                 .to("log:?level=INFO&showBody=true")
                 .process(new SimpleJDBCInsertProcessor())
                 .to("jdbc:myDataSource")
